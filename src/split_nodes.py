4338d29af6b0ac6c648ddef391f623f596303889
ReallySoggySocks <havenkain@gmail.com>
2025-03-08 13:11:58 -0600

    completed split_nodes.py

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,10 +1,24 @@
 from textnode import TextNode, TextType
 
-node = TextNode("This is a **bold** word", TextType.TEXT)
-
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    for word in old_nodes.text.split():
-        if delimiter in word:
-            text_type = TextType[delimiter]
-    
-    return text_type
+
+    new_list = []
+
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_list.append(node)
+        else:
+            new_nodes = node.text.split(delimiter)
+            if len(new_nodes) % 2 != 0:
+                raise Exception("Unevern Delimiter - Invalid Markdown Syntax")
+
+            for i, text in enumerate(new_nodes):
+                if not text.strip():
+                    continue
+
+                if i %2 == 0:
+                    new_list.append(TextNode(text, TextType.TEXT))
+                else:
+                    new_list.append(TextNode(text, text_type))
+
+    return new_list
